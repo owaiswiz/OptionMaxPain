4fb1b17cfca7af22affe44030c693b0f0ba433fd
Owais <owaiswiz@gmail.com>
2018-07-22 01:41:37 +0530

    Add implementation for OptionMaxPainCalculator

--- lib/option_max_pain/calculator.rb
+++ lib/option_max_pain/calculator.rb
@@ -6,7 +6,21 @@ module OptionMaxPain
       # Eg =>
       # options = {7700 => {call: 2131, put: 1234}, 7800 => {call: 890, put: 8209183}}
 
-      options
+      options_pain = {}
+      sorted_strikes = options.keys.sort
+
+      sorted_strikes.each_with_index do |strike, index| 
+        loss_due_to_call = sorted_strikes[0..index].collect do |key|
+          options[key][:call] * (strike - key)
+        end.sum
+
+        loss_due_to_put = sorted_strikes[index..-1].collect do |key|
+          options[key][:put] * (key - strike)
+        end.sum
+
+        options_pain[strike] = loss_due_to_call + loss_due_to_put
+      end
+      [options_pain.min_by {|k,v| v}].to_h
     end
   end
 end
